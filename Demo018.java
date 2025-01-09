1b4a0b9e92ec1a44e609fdec3d5a8305dcf1a0f3
sindhusudanaboina <sindhusudhanaboina0711@gmail.com>
2025-01-09 11:31:38 +0530

    Add files via upload

--- Demo018.java
+++ Demo018.java
@@ -1,19 +1,32 @@
 import java.util.Scanner;
-
 public class Demo018 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter a number: ");
-        int number = scanner.nextInt();
-
-        // Check if the number is a multiple of 100
-        if (number % 100 == 0) {
-            System.out.println(number + " is a multiple of 100.");
-        } else {
-            System.out.println(number + " is not a multiple of 100.");
+    
+    
+    
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            
+           
+            System.out.print("Enter the number of minutes: ");
+            long totalMinutes = scanner.nextLong();
+            
+           
+            long minutesInAnHour = 60;
+            long hoursInADay = 24;
+            long daysInAYear = 365;
+            
+            long minutesInADay = minutesInAnHour * hoursInADay;
+            long minutesInAYear = minutesInADay * daysInAYear; 
+            
+           
+            long years = totalMinutes / minutesInAYear;
+            long remainingMinutesAfterYears = totalMinutes % minutesInAYear;
+            long days = remainingMinutesAfterYears / minutesInADay;
+            long remainingMinutes = remainingMinutesAfterYears % minutesInADay;
+            
+            
+            System.out.println(totalMinutes + " minutes is approximately " + years + " years, " + days + " days, and " + remainingMinutes + " minutes.");
+            
+            scanner.close();
         }
-
-        scanner.close();
-    }
 }
