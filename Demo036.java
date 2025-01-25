599fb89a1b784c43928790a1dfe22422ae71cc80
sindhusudanaboina <sindhusudhanaboina0711@gmail.com>
2025-01-25 10:06:58 +0530

    Add files via upload

---
+++ Demo036.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Demo036 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("Enter third number: ");
+        int num3 = scanner.nextInt();
+
+        if (num1 >= num2 && num1 >= num3) {
+            System.out.println("The biggest number is: " + num1);
+        } else if (num2 >= num1 && num2 >= num3) {
+            System.out.println("The biggest number is: " + num2);
+        } else {
+            System.out.println("The biggest number is: " + num3);
+        }
+
+        scanner.close();
+    }
+}
